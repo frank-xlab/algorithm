84953a4f215d7c7d3a599516bdbb3dae2469ce69
jipeng <jipeng.lab@gmail.com>
2022-01-05 23:03:36 +0800

    init project

---
+++ rust/src/search/binary.rs
@@ -0,0 +1,47 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+use std::collections::VecDeque;
+
+// 数据结构：栈、队列、链表、树、图
+// 算法：
+// 设计模式:
+
+// 每学一个语言，都要从数据结构，算法到设计模式实践一遍
+// 堆栈链表图
+// 中间件，Server，Command Tools
+
+// Sequences: Vec, VecDeque, LinkedList
+// Maps: HashMap, BTreeMap
+// Sets: HashSet, BTreeSet
+// Misc: BinaryHeap
+
+//pub struct LinkedList<T> {
+//    head: Option<NonNull<Node<T>>>,
+//    tail: Option<NonNull<Node<T>>>,
+//    len: usize,
+//    marker: PhantomData<Box<Node<T>>>,
+//}
+//
+//struct Node<T> {
+//    next: Option<NonNull<Node<T>>>,
+//    prev: Option<NonNull<Node<T>>>,
+//    element: T,
+//}
+
+#[cfg(test)]
+mod tests {
+    use super::projected_rect_contains;
+    use euclid::{rect, Transform3D};
+
+    #[test]
+    fn test_s() {}
+}
+
+// 二分查找
+//
+//
+
+// TODO:RustFmt
+// :RustTest
