996921d886579ac197e00684510bfd3be62da651
凤舞 <jipeng.lab@gmail.com>
2023-02-12 20:51:20 +0800

    fix: push

--- rust/src/collection/linked_list.rs
+++ rust/src/collection/linked_list.rs
@@ -1,51 +1,76 @@
-//! ssss
+//! Linked list
 //!
-//!
 
 
-// LinkedList 本身是个类型，当定义为变量的时候，需要给这个变量分配内存，该变量分配的内存是需要明确的，但不用管变量内存存储的是什么内容
-// LinkedList 定义这个类型的变量的时候，需要给这个变量分配内容，比如32位整数类型定义变量的时候，需要分配一段32的内存
+type Link<T> = Option<Box<Node<T>>>
 
-type Link<T> = Option<Box<Node<T>>>;
-
-pub struct LinkedList<T> {
-  head: Option<Box<Node<T>>>,
+struct Node<T> {
+  element: T,
+  next: Link<T>
 }
 
-pub struct Node<T> {
-  element: T,
-  next: Option<Box<Node<T>>>,
+pub struct LinkedList<T> {
+  head: Link<T>
+  len: usize
 }
 
 impl<T> LinkedList<T> {
+
   pub fn new() -> Self {
-    LinkedList { head: None,}
+    LinkedList { head: None }
   }
 
-  pub fn push(&mut self, element: T) {
-    let fresh: Box<Node<T>> = Box::new(Node {
-      element: elem,
-      next: self.head.take()
-    });
-
-    self.head = Some(fresh)
+  pub fn push(&mut self, element: T) -> Self {
+    self.head.take()
   }
 
-  pub fn pop() -> Option<T> {
+  pub fn pop(&mut self) -> Option<T> {
 
   }
+
+  pub fn remove(&mut self, index: usize) ->Option<T> {
+
+  }
+
+  pub fn find(&mut self, callback:Fn) -> Option<T> {
+
+  }
+
 }
 
-impl<T> Drop for LinkedList<T> {
-  drop(LinkedList)
-}
-
-[#cfg(test)]
+#[cfg(test)]
 mod tests {
 
-  #[test]
-  fn initial() {
-    const linked_list = LinkedList::new();
-  }
+    #[test]
+    fn initial() {
+        let list = LinkedList::<String>::new();
 
+        list.push(String::from("hello world!"));
+
+        let last_element = list.pop();
+        // TODO:增加Debug实现
+        println!("{}", last_element);
+        
+        fn callback(&element: String, index) {
+          if (element.value === 0) {
+            index
+          }
+        }
+
+        let index = list.findIndex(callback);
+        let element = list.find(callback);
+        // TODO: some/none
+        match element {
+          None => println!("Nothing"),
+          Some => println!("{}", element.value),
+        }
+
+        list.insert(5,String::from("Good Luck!"));
+
+        // TODO: 超出边界需要提示错误
+        let result = list.remove(3).unwrap();
+        println!("{}", result);
+
+        list.map(|x| x.value).unwrap();
+    }
 }
